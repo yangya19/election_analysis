4ec4c7c7dce6a5348ecbdfb96d96098db7cfc825
yangya19 <95553242+yangya19@users.noreply.github.com>
2022-01-18 17:04:11 -0500

    Adding the election audit code

--- Pyroll.py
+++ Pyroll.py
@@ -14,12 +14,68 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file.
+
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 with open(file_to_load) as election_data:
     # To do: read and analyze the data here.
     file_reader = csv.reader(election_data)
 
     #Read and print the header row
     headers = next(file_reader)
-    print(headers)
+    
+    #Print each row in the CSV file
+    for row in file_reader:
+
+        total_votes += 1
+
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+
+            candidate_options.append(candidate_name)
+
+            candidate_votes[candidate_name] = 0
+
+        candidate_votes[candidate_name] += 1
+
+
+print(candidate_options)
+print(candidate_votes)
+
+for candidate_name in candidate_votes:
+
+    votes = candidate_votes[candidate_name]
+
+    vote_percentages = float(votes)/float(total_votes) * 100
+
+    if (votes > winning_count) and (vote_percentages > winning_percentage):
+
+        winning_count = votes
+
+        winning_percentage = vote_percentages
+
+        winning_candidate = candidate_name
+
+    print(f"{candidate_name}: {vote_percentages:.1f}% ({votes:,})\n")
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+
+
+
+
+
+
         
     
